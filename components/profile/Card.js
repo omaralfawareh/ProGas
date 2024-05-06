bb38401438d2d4ac47edd6d0bbb38be63ebb5383
Omar <alfawareho@gmail.com>
2024-05-07 00:58:02 +0300

    feat(account profile)

---
+++ components/profile/Card.js
@@ -0,0 +1,56 @@
+import { Card as RCard } from "@rneui/themed";
+import { Avatar } from "@rneui/themed";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+
+function Card(props) {
+  const { text, icon, onPress, color, extraStyle } = props;
+  return (
+    <TouchableOpacity style={{ width: "100%" }} onPress={onPress}>
+      <RCard containerStyle={style.card} rounded>
+        <View style={{ flexDirection: "row", gap: 10, alignItems: "center" }}>
+          <Avatar
+            size={35}
+            rounded
+            icon={{
+              name: icon,
+              type: "material",
+            }}
+            containerStyle={{ backgroundColor: "green" }}
+          />
+          <Text
+            style={{
+              fontSize: 15,
+              color: color,
+              ...extraStyle,
+              //   borderWidth: 1,
+              //   borderColor: "black",
+            }}
+          >
+            {text}
+          </Text>
+        </View>
+      </RCard>
+    </TouchableOpacity>
+  );
+}
+export default Card;
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    borderWidth: 2,
+    borderColor: "red",
+    padding: 10,
+    alignItems: "center",
+    gap: 10,
+  },
+  inputField: {},
+  button: {
+    width: "100%",
+  },
+  title: { textAlign: "left", fontWeight: "bold" },
+  card: {
+    borderRadius: 13,
+    width: "100%",
+    margin: "0",
+  },
+});
--- screens/profile.js
+++ screens/profile.js
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { useContext } from "react";
-import { Button } from "native-base";
 import AuthContext from "../store/auth-context";
-import { Card } from "@rneui/themed";
-import { Avatar } from "@rneui/themed";
+import { Avatar, BottomSheet, Card as RCard, Icon } from "@rneui/themed";
+import { Input, Button } from "native-base";
+
+import Card from "../components/profile/Card";
 function Profile() {
   const authCtx = useContext(AuthContext);
+  const [isAccountVisible, setIsAcountVisible] = useState(false);
 
   return (
     <View style={style.container}>
@@ -15,6 +18,7 @@ function Profile() {
           alignItems: "center",
           flex: 0.6,
           justifyContent: "center",
+          gap: 0,
         }}
       >
         <Avatar
@@ -24,25 +28,89 @@ function Profile() {
             uri: "https://cdn-icons-png.freepik.com/512/147/147142.png?ga=GA1.1.1980490144.1701362827",
           }}
         ></Avatar>
-        <Text style={{ fontSize: 20 }}>Omar Alfawareh</Text>
+        <Text style={{ ...style.title, fontSize: 25 }}>Omar Alfawareh</Text>
+        <Text style={{}}>alfawareho@gmail.com</Text>
       </View>
-      <Card containerStyle={style.card} rounded>
-        <Card.Title style={style.title}> This is a test Card</Card.Title>
-        <Card.Divider />
-        <Text>Omar is the best</Text>
-      </Card>
-      <Card containerStyle={style.card}>
-        <Card.Title style={style.title}> This is a test Card</Card.Title>
-        <Card.Divider />
-        <Text>Omar is the best</Text>
-      </Card>
-      <Button
+      <View
+        style={{
+          width: "100%",
+          // borderWidth: 2,
+          //  borderColor: "red",
+        }}
+      >
+        <Text style={{ ...style.title, fontSize: 20 }}>Settings</Text>
+      </View>
+      <Card
+        text="Account Information"
+        icon="person"
+        color="black"
+        onPress={() => setIsAcountVisible(true)}
+      />
+      <BottomSheet
+        isVisible={isAccountVisible}
+        onBackdropPress={() => setIsAcountVisible(false)}
+        containerStyle={{
+          borderWidth: 2,
+          borderColor: "red",
+        }}
+      >
+        <RCard containerStyle={style.card}>
+          <Text style={{ ...style.title, marginBottom: 10, fontSize: 20 }}>
+            Account Information
+          </Text>
+          <Input
+            size="lg"
+            style={style.inputField}
+            type="text"
+            placeholder="Full Name"
+            value={"Omar Alfawareh"}
+            isReadOnly={true}
+          />
+          <Input
+            size="lg"
+            style={style.inputField}
+            type="text"
+            placeholder="Email"
+            value={"alfawareho@gmail.com"}
+            onChangeTex
+            isReadOnly={true}
+          />
+          <Input
+            size="lg"
+            style={style.inputField}
+            type="text"
+            placeholder="Phone Number"
+            value={"0791141046"}
+            isReadOnly={true}
+          />
+          <View style={{ alignItems: "center" }}>
+            <Button
+              borderRadius="full"
+              colorScheme="success"
+              onPress={() => setIsAcountVisible(false)}
+              style={{ marginTop: 200, width: 55, height: 55 }}
+            >
+              <Icon name="close" type="material" />
+            </Button>
+          </View>
+        </RCard>
+      </BottomSheet>
+      <Card text="Change Password" icon="lock" color="black" />
+      <Card text="FAQ" icon="help" color="black" />
+      <Card
+        text="Logout"
+        icon="logout"
+        onPress={authCtx.signOut}
+        color="red"
+        extraStyle={{ fontWeight: "bold" }}
+      />
+      {/* <Button
         style={style.button}
         colorScheme="success"
         onPress={authCtx.signOut}
       >
         Logout
-      </Button>
+      </Button> */}
     </View>
   );
 }
@@ -50,16 +118,27 @@ export default Profile;
 const style = StyleSheet.create({
   container: {
     flex: 1,
-    borderWidth: 2,
-    borderColor: "red",
+    // borderWidth: 2,
+    // borderColor: "red",
     padding: 10,
     alignItems: "center",
-    gap: 15,
+    gap: 10,
   },
-  inputField: {},
   button: {
     width: "100%",
   },
-  title: { textAlign: "left" },
-  card: { borderRadius: 13, width: "100%", margin: "0" },
+  title: { textAlign: "left", fontWeight: "bold" },
+  card: {
+    width: "100%",
+    margin: "0",
+    // paddingBottom: 200,
+    borderRadius: 13,
+    borderColor: "blue",
+    padding: 10,
+  },
+  inputField: {
+    backgroundColor: "white",
+    borderRadius: 13,
+    marginBottom: 15,
+  },
 });
